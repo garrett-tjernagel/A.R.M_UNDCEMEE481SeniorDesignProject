a26e1eafc72a155fc02051336e293efedc4d56f6
garrett-tjernagel <102770503+garrett-tjernagel@users.noreply.github.com>
2022-04-12 08:42:21 -0500

    Update RadioTransmitterTestingTransmitter.ino

--- GauntletControllerRadioTesting/RadioTransmitterTestingTransmitter/RadioTransmitterTestingTransmitter.ino
+++ GauntletControllerRadioTesting/RadioTransmitterTestingTransmitter/RadioTransmitterTestingTransmitter.ino
@@ -13,7 +13,7 @@ const byte slaveAddress[5] = {'R','x','A','A','A'};
 
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 
-char dataToSend[10] = "Message 0";
+int dataToSend[10]={15,45,75,105,120,15,45,75,105,120};
 char txNum = '0';
 
 
@@ -25,7 +25,7 @@ unsigned long txIntervalMillis = 1000; // send once per second
 void setup() {
 
     Serial.begin(9600);
-
+while(!Serial.available()){}
     Serial.println("SimpleTx Starting");
 
     radio.begin();
@@ -47,14 +47,13 @@ void loop() {
 //====================
 
 void send() {
-dataToSend=Serial.read();
     bool rslt;
     rslt = radio.write( &dataToSend, sizeof(dataToSend) );
         // Always use sizeof() as it gives the size as the number of bytes.
         // For example if dataToSend was an int sizeof() would correctly return 2
 
     Serial.print("Data Sent ");
-    Serial.print(dataToSend);
+    Serial.print(dataToSend[txNum]);
     if (rslt) {
         Serial.println("  Acknowledge received");
         updateMessage();
@@ -69,8 +68,8 @@ dataToSend=Serial.read();
 void updateMessage() {
         // so you can see that new data is being sent
     txNum += 1;
-    if (txNum > '9') {
+    if (txNum > '10') {
         txNum = '0';
     }
-    dataToSend[8] = txNum;
+    dataToSend[10] = txNum;
 }
