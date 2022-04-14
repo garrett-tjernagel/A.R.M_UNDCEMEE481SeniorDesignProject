54f469cdb254752ff2cc2c7d455e2d89128f780d
garrett-tjernagel <102770503+garrett-tjernagel@users.noreply.github.com>
2022-04-14 17:21:43 -0500

    Pretty Consistent dual gyroscope positional data
    added functionality for the second gyroscope, data drifts much less now. Will implement a filter in the future.

--- GauntletControllerv2/GauntletControllerv2.ino
+++ GauntletControllerv2/GauntletControllerv2.ino
@@ -12,26 +12,44 @@ int samplingTime = 1; //Enter time in ms
 
 float gwxrs1, gwyrs1, gwzrs1 = 0;
 float gwxrms1, gwyrms1, gwzrms1 = 0;
-unsigned long deltaTime, to, tc = 0;
 float xrc1, xrd1 = 0;
 float yrc1, yrd1 = 0;
 float zrc1, zrd1 = 0;
+
+float gwxrs2, gwyrs2, gwzrs2 = 0;
+float gwxrms2, gwyrms2, gwzrms2 = 0;
+float xrc2, xrd2 = 0;
+float yrc2, yrd2 = 0;
+float zrc2, zrd2 = 0;
+
 
 float x1RotOffset = 0.040768759;
 float y1RotOffset = -0.048155658;
 float z1RotOffset = -0.0491477;
 
+float x2RotOffset = -0.951493925;
+float y2RotOffset = 0.008503545;
+float z2RotOffset = -0.021624156;
+
+unsigned long deltaTime = 0;
 unsigned long prevTime = 0;
 
 void setup(void) {
   //Reset Variables
   //Gyro 1 (Shoulder)
-float gwxrs1, gwyrs1, gwzrs1 = 0;
-float gwxrms1, gwyrms1, gwzrms1 = 0;
-unsigned long deltaTime, to, tc = 0;
-float xrc1, xrd1 = 0;
-float yrc1, yrd1 = 0;
-float zrc1, zrd1 = 0;
+  float gwxrs1, gwyrs1, gwzrs1 = 0;
+  float gwxrms1, gwyrms1, gwzrms1 = 0;
+  unsigned long deltaTime, to, tc = 0;
+  float xrc1, xrd1 = 0;
+  float yrc1, yrd1 = 0;
+  float zrc1, zrd1 = 0;
+
+  //Wrist
+  float gwxrs2, gwyrs2, gwzrs2 = 0;
+  float gwxrms2, gwyrms2, gwzrms2 = 0;
+  float xrc2, xrd2 = 0;
+  float yrc2, yrd2 = 0;
+  float zrc2, zrd2 = 0;
 
 
 
@@ -43,8 +61,14 @@ float zrc1, zrd1 = 0;
   Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
-  if (!mpu1.begin(0x68) and !mpu2.begin(0x69)) {
-    Serial.println("Failed to find MPU6050 chip");
+  if (!mpu1.begin(0x68)) {
+    Serial.println("Failed to find Gyro1");
+    while (1) {
+      delay(10);
+    }
+  }
+  if (!mpu2.begin(0x69)) {
+    Serial.println("Failed to find Gyro2");
     while (1) {
       delay(10);
     }
@@ -67,10 +91,27 @@ float zrc1, zrd1 = 0;
     case MPU6050_RANGE_16_G:
       Serial.println("+-16G");
       break;
+
   }
   mpu1.setGyroRange(MPU6050_RANGE_500_DEG);
   Serial.print("Gyro range set to: ");
   switch (mpu1.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+  mpu2.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu2.getGyroRange()) {
     case MPU6050_RANGE_250_DEG:
       Serial.println("+- 250 deg/s");
       break;
@@ -118,63 +159,133 @@ float zrc1, zrd1 = 0;
 }
 
 void loop() {
-  /* Get new sensor events with the readings */
-  sensors_event_t a1, g1, temp1;
-  mpu1.getEvent(&a1, &g1, &temp1);
 
-  //calculate average velocity between time samples
-  long newTime = millis();
-  deltaTime = (newTime - prevTime);
+  if (millis() < 1500) {
+    zeroSystem();
+  } else {
+    /* Get new sensor events with the readings */
+    sensors_event_t a1, g1, temp1;
+    sensors_event_t a2, g2, temp2;
+    mpu1.getEvent(&a1, &g1, &temp1);
+    mpu2.getEvent(&a2, &g2, &temp2);
 
-  Serial.print("\t");
-  Serial.print("System Time (ms:)");
-  Serial.print(deltaTime);
-  Serial.print("\t");
+    //calculate average velocity between time samples
+    long newTime = millis();
+    deltaTime = (newTime - prevTime);
+    /*
+        Serial.print("\t");
+        Serial.print("System Time (ms:)");
+        Serial.print(deltaTime);
+        Serial.print("\t");
+    */
 
-  gwxrs1 = g1.gyro.x + x1RotOffset;
-  gwyrs1 = g1.gyro.y + y1RotOffset;
-  gwzrs1 = g1.gyro.z + z1RotOffset;
-  //Serial.print("Gyro W: X (rad/s): ");
-  //Serial.print(gwxrs1, 10);
-  //Serial.print("\t");
-  //Serial.print(gwyrs1, 10);
-  //Serial.print("\t");
-  //Serial.print(gwzrs1, 10);
-  //Serial.print("\t");
+    gwxrs1 = g1.gyro.x + x1RotOffset;
+    gwyrs1 = g1.gyro.y + y1RotOffset;
+    gwzrs1 = g1.gyro.z + z1RotOffset;
 
-  gwxrms1 = gwxrs1 / 1000;
-  gwyrms1 = gwyrs1 / 1000;
-  gwzrms1 = gwzrs1 / 1000;
-  /*
-    Serial.print("Gyro W: X (rad/ms): ");
-    Serial.print(gwxrms1, 10);
+    gwxrs2 = g2.gyro.x + x2RotOffset;
+    gwyrs2 = g2.gyro.y + y2RotOffset;
+    gwzrs2 = g2.gyro.z + z2RotOffset;
+
+    /*
+      Serial.print("Gyro W: X (rad/s): ");
+      Serial.print(gwxrs1, 10);
+      Serial.print("\t");
+      Serial.print(gwyrs1, 10);
+      Serial.print("\t");
+      Serial.print(gwzrs1, 10);
+      Serial.print("\t");
+
+      Serial.print("Gyro2 W: X,Y,Z (rad/s): ");
+      Serial.print("\t");
+      Serial.print(gwxrs2, 10);
+      Serial.print("\t");
+      Serial.print(gwyrs2, 10);
+      Serial.print("\t");
+      Serial.print(gwzrs2, 10);
+      Serial.print("\t");
+    */
+
+    gwxrms1 = gwxrs1 / 1000;
+    gwyrms1 = gwyrs1 / 1000;
+    gwzrms1 = gwzrs1 / 1000;
+
+    gwxrms2 = gwxrs2 / 1000;
+    gwyrms2 = gwyrs2 / 1000;
+    gwzrms2 = gwzrs2 / 1000;
+    /*
+      Serial.print("Gyro1 W: X (rad/ms): ");
+      Serial.print(gwxrms1, 10);
+      Serial.print("\t");
+      Serial.print(gwyrms1, 10);
+      Serial.print("\t");
+      Serial.print(gwzrms1, 10);
+      Serial.print("\t");
+
+      Serial.print("Gyro2 W: X (rad/ms): ");
+      Serial.print(gwxrms2, 10);
+      Serial.print("\t");
+      Serial.print(gwyrms2, 10);
+      Serial.print("\t");
+      Serial.print(gwzrms2, 10);
+      Serial.print("\t");
+    */
+
+    xrd1 = (gwxrms1 * deltaTime) * 180 / M_PI;
+    yrd1 = (gwyrms1 * deltaTime) * 180 / M_PI;
+    zrd1 = (gwzrms1 * deltaTime) * 180 / M_PI;
+
+    xrd2 = (gwxrms2 * deltaTime) * 180 / M_PI;
+    yrd2 = (gwyrms2 * deltaTime) * 180 / M_PI;
+    zrd2 = (gwzrms2 * deltaTime) * 180 / M_PI;
+
+    xrc1 = xrc1 + xrd1;
+    yrc1 = yrc1 + yrd1;
+    zrc1 = zrc1 + zrd1;
+
+    xrc2 = xrc2 + xrd2;
+    yrc2 = yrc2 + yrd2;
+    zrc2 = zrc2 + zrd2;
+
+    Serial.print("Gyro1 X,Y,Z (rad): ");
+    Serial.print(xrc1, 4);
     Serial.print("\t");
-    Serial.print(gwyrms1, 10);
+    Serial.print(yrc1, 4);
     Serial.print("\t");
-    Serial.print(gwzrms1, 10);
+    Serial.print(zrc1, 4);
     Serial.print("\t");
-  */
 
-  xrd1 = (gwxrms1 * deltaTime) * 180 / M_PI;
-  yrd1 = (gwyrms1 * deltaTime) * 180 / M_PI;
-  zrd1 = (gwzrms1 * deltaTime) * 180 / M_PI;
+    Serial.print("Gyro2 X,Y,Z (rad): ");
+    Serial.print(xrc2, 4);
+    Serial.print("\t");
+    Serial.print(yrc2, 4);
+    Serial.print("\t");
+    Serial.print(zrc2, 4);
+    Serial.print("\t");
 
-  xrc1 = xrc1 + xrd1;
-  yrc1 = yrc1 + yrd1;
-  zrc1 = zrc1 + zrd1;
-  Serial.print("Gyro X,Y,Z (rad): ");
-  Serial.print(xrc1, 10);
-  Serial.print("\t");
-  Serial.print(yrc1, 10);
-  Serial.print("\t");
-  Serial.print(zrc1, 10);
-  Serial.print("\t");
+    //Looping data points
 
-  //Looping data points
+    prevTime = newTime;
+    void  reset(void);
+    Serial.println();
+  }
+}
 
-  prevTime = newTime;
-  void  reset(void);
-  Serial.println();
+void zeroSystem() {
+  //Gyro 1 (Shoulder)
+  float gwxrs1, gwyrs1, gwzrs1 = 0;
+  float gwxrms1, gwyrms1, gwzrms1 = 0;
+  unsigned long deltaTime, to, tc = 0;
+  float xrc1, xrd1 = 0;
+  float yrc1, yrd1 = 0;
+  float zrc1, zrd1 = 0;
+
+  //Wrist
+  float gwxrs2, gwyrs2, gwzrs2 = 0;
+  float gwxrms2, gwyrms2, gwzrms2 = 0;
+  float xrc2, xrd2 = 0;
+  float yrc2, yrd2 = 0;
+  float zrc2, zrd2 = 0;
 }
 
 /* OLD CODE FROM THE SKETCH: REMOVE AT END
