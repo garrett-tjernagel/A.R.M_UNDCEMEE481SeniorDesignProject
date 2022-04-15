22f01e26159b630a2f87ceb9d44de13cb7791309
garrett-tjernagel <102770503+garrett-tjernagel@users.noreply.github.com>
2022-04-15 15:06:29 -0500

    Code layout

--- A.R.M_V1/A.R.M_V1.ino
+++ A.R.M_V1/A.R.M_V1.ino
@@ -1,3 +1,24 @@
+//=========================================Introduction
+/*Code written by Garrett Tjernagel for
+  UND CEM EE480-481 SP22 Senior Design Capstone Project
+  Augmented Robotic Manipulator (A.R.M)
+  Partners: Branson Elliot and William Prody
+
+*/
+
+//=========================================Sources, Inspiration, and Links
+/*
+  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7309037/
+  https://forum.arduino.cc/t/simple-nrf24l01-2-4ghz-transceiver-demo/405123/2
+  https://www.instructables.com/How-to-use-a-Flex-Sensor-Arduino-Tutorial/
+  https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html#a64e6b74741d31138fb60f14ec2e7d9c1
+  https://arduino.stackexchange.com/questions/86031/adafruit-mpu-6050-and-adafruit-i2c-multiplexer
+  https://forum.arduino.cc/t/integration-of-acceleration/158296/10
+  https://forum.arduino.cc/t/measuring-time/96602/4
+
+
+
+*/
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,3 +28,14 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+/*
+ * 1.Servo objects
+ * 2.Radio setup
+ * 3.Gyro setup
+ * 4.Initialization (Homing)
+ * 5.Internal Data acquisition 
+ * 6.External Controller data acquisition
+ * 7.Rudimentary PID for each servo
+ * 8.Servo movement assignment
+ */
